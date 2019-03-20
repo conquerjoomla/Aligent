405b531d77549db400b0d2a65025a67977133f2e
Nicholas Hamilton <nicholas@zilsoft.com>
2019-03-21 09:31:04 +1030

    Add FormatResult

--- functions.php
+++ functions.php
@@ -1,64 +1,76 @@
 <?php
 
+function FormatResult($Days, $format)
+{
+    switch ($format)
+    {
+    case 1: return $Days * 24 * 60 * 60; // Seconds
+    case 2: return $Days * 24 * 60; // Minutes
+    case 3: return $Days * 24; // Hours
+    case 4: return $Days; // Days
+    case 5: return floor($Days / 365); // Years
+    }
+}
+
 function ValidateDate($date, $format = 'Y-m-d H:i:s')
 {
     $d = DateTime::createFromFormat($format, $date);
-    $valid = DateTime::getLastErrors();         
+    $valid = DateTime::getLastErrors();
     if ($valid['warning_count']!=0 || $valid['error_count']!=0)
-	{
-		return false;
-	}
+    {
+        return false;
+    }
     return $d && $d->format($format) == $date;
 }
 
-function DaysBetween($Start, $End, $format = 'Y-m-d H:i:s')
+function DaysBetween($Start, $End, $ResultFormat = 4, $Dateformat = 'Y-m-d H:i:s')
 {
-	if (!ValidateDate($Start, $format)) { return false; }
-	if (!ValidateDate($End, $format)) { return false; }
-	
-	$StartDate = DateTime::createFromFormat($format,$Start);
-	$EndDate = DateTime::createFromFormat($format,$End);
-	if ($StartDate > $EndDate)
-	{
-		return 0;
-	}
-	$diff = $EndDate->diff($StartDate);
-	return $diff->days;
+    if (!ValidateDate($Start, $Dateformat)) { return false; }
+    if (!ValidateDate($End, $Dateformat)) { return false; }
+
+    $StartDate = DateTime::createFromFormat($Dateformat,$Start);
+    $EndDate = DateTime::createFromFormat($Dateformat,$End);
+    if ($StartDate > $EndDate)
+    {
+        return 0;
+    }
+    $diff = $EndDate->diff($StartDate);
+    return FormatResult($diff->days, $ResultFormat);
 }
 
-function WeeksBetween($Start, $End, $format = 'Y-m-d H:i:s')
+function WeeksBetween($Start, $End, $ResultFormat = 4, $Dateformat = 'Y-m-d H:i:s')
 {
-	$Days = DaysBetween($Start, $End, $format);
-	if ($Days === false)
-	{
-		return false;
-	} 
-	return floor($Days / 7);
+    $Days = DaysBetween($Start, $End, 4, $Dateformat);
+    if ($Days === false)
+    {
+        return false;
+    }
+    return FormatResult(floor($Days / 7), $ResultFormat);
 }
 
-function WeekdaysBetween($Start, $End, $format = 'Y-m-d H:i:s')
+function WeekdaysBetween($Start, $End, $ResultFormat = 4, $Dateformat = 'Y-m-d H:i:s')
 {
-	$Days = DaysBetween($Start, $End, $format);
-	if ($Days === false)
-	{
-		return false;
-	} 
+    $Days = DaysBetween($Start, $End, 4, $Dateformat);
+    if ($Days === false)
+    {
+        return false;
+    }
 
-	$StartDate = DateTime::createFromFormat($format,$Start);
-	$EndDate = DateTime::createFromFormat($format,$End);
-	if ($StartDate > $EndDate)
-	{
-		return 0;
-	}
-		
-	$Days = 0;
-	$end = intval($EndDate->format('U'));
+    $StartDate = DateTime::createFromFormat($Dateformat,$Start);
+    $EndDate = DateTime::createFromFormat($Dateformat,$End);
+    if ($StartDate > $EndDate)
+    {
+        return 0;
+    }
+
+    $Days = 0;
+    $end = intval($EndDate->format('U'));
     for ($Day = intval($StartDate->format('U')); $Day <= $end; $Day += 86400)
-	{
-		if (date("N", $Day) <= 5) 
-		{
-			$Days += 1;
-		};
-	}
-	return $Days;
+    {
+        if (date("N", $Day) <= 5)
+        {
+            $Days += 1;
+        };
+    }
+    return FormatResult($Days,$ResultFormat);
 }
--- tests/test.php
+++ tests/test.php
@@ -6,97 +6,111 @@ require_once(dirname(dirname(__FILE__)).'/functions.php');
 
 final class AligentTest extends TestCase
 {
+    public function testFormatResult(): void
+    {
+        $this->assertEquals(FormatResult(1,1),86400);
+        $this->assertEquals(FormatResult(1,2),1440);
+        $this->assertEquals(FormatResult(1,3),24);
+        $this->assertEquals(FormatResult(1,4),1);
+        $this->assertEquals(FormatResult(1,5),0);
+        $this->assertEquals(FormatResult(400,1),34560000);
+        $this->assertEquals(FormatResult(400,2),576000);
+        $this->assertEquals(FormatResult(400,3),9600);
+        $this->assertEquals(FormatResult(400,4),400);
+        $this->assertEquals(FormatResult(400,5),1);
+    }
+
     public function testValidDate(): void
     {
-		$this->assertTrue(validateDate('2019-02-28','Y-m-d'));
-		$this->assertFalse(validateDate('2019-02-30','Y-m-d'));
-		$this->assertFalse(validateDate('aaaa','Y-m-d'));
-		$this->assertFalse(validateDate(null,'Y-m-d'));
-		$this->assertFalse(validateDate('2019-01-01 12:00:00','Y-m-d'));
-		$this->assertTrue(validateDate('2019-01-01 12:00:00','Y-m-d H:i:s'));
-		$this->assertTrue(validateDate('2019-01-01','Y-m-d'));
-		$this->assertFalse(validateDate('2019-01-1','Y-m-d'));
-		$this->assertTrue(validateDate('2019-01-1','Y-m-j'));
-		$this->assertFalse(validateDate('2019-1-1','Y-m-j'));
-		$this->assertTrue(validateDate('2019-1-1','Y-n-j'));
+        $this->assertTrue(validateDate('2019-02-28','Y-m-d'));
+        $this->assertFalse(validateDate('2019-02-30','Y-m-d'));
+        $this->assertFalse(validateDate('aaaa','Y-m-d'));
+        $this->assertFalse(validateDate(null,'Y-m-d'));
+        $this->assertFalse(validateDate('2019-01-01 12:00:00','Y-m-d'));
+        $this->assertTrue(validateDate('2019-01-01 12:00:00','Y-m-d H:i:s'));
+        $this->assertTrue(validateDate('2019-01-01','Y-m-d'));
+        $this->assertFalse(validateDate('2019-01-1','Y-m-d'));
+        $this->assertTrue(validateDate('2019-01-1','Y-m-j'));
+        $this->assertFalse(validateDate('2019-1-1','Y-m-j'));
+        $this->assertTrue(validateDate('2019-1-1','Y-n-j'));
     }
 
     public function testDaysBetween(): void
     {
-		$this->assertEquals(DaysBetween('2019-6-24', '2019-6-26', 'Y-n-j'),2);
-		$this->assertEquals(DaysBetween('2019-3-1', '2019-7-1', 'Y-n-j'),122);
-		$this->assertEquals(DaysBetween('2019-6-26', '2019-6-24', 'Y-n-j'),0);
-		$this->assertEquals(DaysBetween('2019-2-24', '2019-3-1', 'Y-n-j'),5);
-		$this->assertEquals(DaysBetween('2020-2-24', '2020-3-1', 'Y-n-j'),6);
-		$this->assertEquals(DaysBetween('2019-1-1', '2020-1-1', 'Y-n-j'),365);
-		$this->assertEquals(DaysBetween('2016-1-1', '2017-1-1', 'Y-n-j'),366);
-		$this->assertEquals(DaysBetween('2000-1-1', '2001-1-1', 'Y-n-j'),366);
-		$this->assertEquals(DaysBetween('2100-1-1', '2101-1-1', 'Y-n-j'),365);
+        $this->assertEquals(DaysBetween('2019-6-24', '2019-6-26', 4, 'Y-n-j'),2);
+        $this->assertEquals(DaysBetween('2019-3-1', '2019-7-1', 4, 'Y-n-j'),122);
+        $this->assertEquals(DaysBetween('2019-6-26', '2019-6-24', 4, 'Y-n-j'),0);
+        $this->assertEquals(DaysBetween('2019-2-24', '2019-3-1', 4, 'Y-n-j'),5);
+        $this->assertEquals(DaysBetween('2020-2-24', '2020-3-1', 4, 'Y-n-j'),6);
+        $this->assertEquals(DaysBetween('2019-1-1', '2020-1-1', 4, 'Y-n-j'),365);
+        $this->assertEquals(DaysBetween('2016-1-1', '2017-1-1', 4, 'Y-n-j'),366);
+        $this->assertEquals(DaysBetween('2000-1-1', '2001-1-1', 4, 'Y-n-j'),366);
+        $this->assertEquals(DaysBetween('2100-1-1', '2101-1-1', 4, 'Y-n-j'),365);
     }
-	
-	public function testWeekdaysBetween(): void
-	{
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-1', 'Y-n-j'),1);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-2', 'Y-n-j'),1);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-3', 'Y-n-j'),1);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-4', 'Y-n-j'),2);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-5', 'Y-n-j'),3);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-6', 'Y-n-j'),4);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-7', 'Y-n-j'),5);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-8', 'Y-n-j'),6);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-9', 'Y-n-j'),6);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-10', 'Y-n-j'),6);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-11', 'Y-n-j'),7);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-12', 'Y-n-j'),8);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-13', 'Y-n-j'),9);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-14', 'Y-n-j'),10);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-15', 'Y-n-j'),11);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-16', 'Y-n-j'),11);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-17', 'Y-n-j'),11);
-		$this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-18', 'Y-n-j'),12);
 
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-2', 'Y-n-j'),0);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-3', 'Y-n-j'),0);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-4', 'Y-n-j'),1);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-5', 'Y-n-j'),2);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-6', 'Y-n-j'),3);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-7', 'Y-n-j'),4);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-8', 'Y-n-j'),5);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-9', 'Y-n-j'),5);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-10', 'Y-n-j'),5);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-11', 'Y-n-j'),6);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-12', 'Y-n-j'),7);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-13', 'Y-n-j'),8);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-14', 'Y-n-j'),9);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-15', 'Y-n-j'),10);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-16', 'Y-n-j'),10);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-17', 'Y-n-j'),10);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-18', 'Y-n-j'),11);
-		$this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-19', 'Y-n-j'),12);
+    public function testWeekdaysBetween(): void
+    {
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-1', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-2', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-3', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-4', 4, 'Y-n-j'),2);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-5', 4, 'Y-n-j'),3);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-6', 4, 'Y-n-j'),4);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-7', 4, 'Y-n-j'),5);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-8', 4, 'Y-n-j'),6);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-9', 4, 'Y-n-j'),6);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-10', 4, 'Y-n-j'),6);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-11', 4, 'Y-n-j'),7);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-12', 4, 'Y-n-j'),8);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-13', 4, 'Y-n-j'),9);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-14', 4, 'Y-n-j'),10);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-15', 4, 'Y-n-j'),11);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-16', 4, 'Y-n-j'),11);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-17', 4, 'Y-n-j'),11);
+        $this->assertEquals(WeekdaysBetween('2019-3-1', '2019-3-18', 4, 'Y-n-j'),12);
 
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-6', 'Y-n-j'),1);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-7', 'Y-n-j'),2);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-8', 'Y-n-j'),3);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-9', 'Y-n-j'),3);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-10', 'Y-n-j'),3);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-11', 'Y-n-j'),4);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-12', 'Y-n-j'),5);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-13', 'Y-n-j'),6);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-14', 'Y-n-j'),7);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-15', 'Y-n-j'),8);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-16', 'Y-n-j'),8);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-17', 'Y-n-j'),8);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-18', 'Y-n-j'),9);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-19', 'Y-n-j'),10);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-20', 'Y-n-j'),11);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-21', 'Y-n-j'),12);
-		$this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-22', 'Y-n-j'),13);
-	}
-	
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-2', 4, 'Y-n-j'),0);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-3', 4, 'Y-n-j'),0);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-4', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-5', 4, 'Y-n-j'),2);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-6', 4, 'Y-n-j'),3);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-7', 4, 'Y-n-j'),4);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-8', 4, 'Y-n-j'),5);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-9', 4, 'Y-n-j'),5);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-10', 4, 'Y-n-j'),5);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-11', 4, 'Y-n-j'),6);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-12', 4, 'Y-n-j'),7);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-13', 4, 'Y-n-j'),8);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-14', 4, 'Y-n-j'),9);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-15', 4, 'Y-n-j'),10);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-16', 4, 'Y-n-j'),10);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-17', 4, 'Y-n-j'),10);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-18', 4, 'Y-n-j'),11);
+        $this->assertEquals(WeekdaysBetween('2019-3-2', '2019-3-19', 4, 'Y-n-j'),12);
+
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-6', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-7', 4, 'Y-n-j'),2);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-8', 4, 'Y-n-j'),3);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-9', 4, 'Y-n-j'),3);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-10', 4, 'Y-n-j'),3);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-11', 4, 'Y-n-j'),4);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-12', 4, 'Y-n-j'),5);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-13', 4, 'Y-n-j'),6);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-14', 4, 'Y-n-j'),7);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-15', 4, 'Y-n-j'),8);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-16', 4, 'Y-n-j'),8);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-17', 4, 'Y-n-j'),8);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-18', 4, 'Y-n-j'),9);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-19', 4, 'Y-n-j'),10);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-20', 4, 'Y-n-j'),11);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-21', 4, 'Y-n-j'),12);
+        $this->assertEquals(WeekdaysBetween('2019-3-6', '2019-3-22', 4, 'Y-n-j'),13);
+    }
+
     public function testWeeksBetween(): void
     {
-		$this->assertEquals(WeeksBetween('2019-3-1', '2019-3-5', 'Y-n-j'),0);		
-		$this->assertEquals(WeeksBetween('2019-3-1', '2019-3-8', 'Y-n-j'),1);		
-		$this->assertEquals(WeeksBetween('2019-1-1', '2019-12-31', 'Y-n-j'),52);		
-	}
+        $this->assertEquals(WeeksBetween('2019-3-1', '2019-3-5', 4, 'Y-n-j'),0);
+        $this->assertEquals(WeeksBetween('2019-3-1', '2019-3-8', 4, 'Y-n-j'),1);
+        $this->assertEquals(WeeksBetween('2019-1-1', '2019-12-31', 4, 'Y-n-j'),52);
+    }
 }
